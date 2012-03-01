9f817ce741f2192c2dec0c5cc35a0795c92c2c4e
Nick Rogers <ncrogers@gmail.com>
2012-03-01 16:14:33 -0500

    Sync with shopify/master

--- test/unit/integrations/authorize_net_sim_module_test.rb
+++ test/unit/integrations/authorize_net_sim_module_test.rb
@@ -1,9 +1,9 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
-require File.dirname(__FILE__) + '/action_view_helper_test'
+require 'test_helper'
 
-class AuthorizeNetSimModuleTest < ActionViewHelperTest
+class AuthorizeNetSimModuleTest < Test::Unit::TestCase
+  include ActionViewHelperTestHelper
   include ActiveMerchant::Billing::Integrations
-  
+
   def test_notification_method
     assert_instance_of AuthorizeNetSim::Notification, AuthorizeNetSim.notification('name=cody')
   end
@@ -12,10 +12,10 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
 	    service.billing_address :address1 => 'address1', :address2 => 'line 2'
     }
-    all= ['<input id="x_address" name="x_address" type="hidden" value="address1 line 2" />']    
+    all= ['<input id="x_address" name="x_address" type="hidden" value="address1 line 2" />']
     check_inclusion all
   end
-  
+
   def test_lots_of_line_items_same_name
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
       35.times {service.add_line_item :name => 'beauty2 - ayoyo', :quantity => 1, :unit_price => 0}
@@ -24,7 +24,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     # It should display them all in, despite each having the same name.
     assert @output_buffer.scan(/beauty2 - ayoyo/).length > 5
   end
-  
+
   def test_lots_of_line_items_different_names
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
       35.times {|n| service.add_line_item :name => 'beauty2 - ayoyo' + n.to_s, :quantity => 1, :unit_price => 0}
@@ -32,14 +32,14 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     assert @output_buffer =~ / ayoyo3/
     assert @output_buffer =~ / ayoyo4/
   end
-  
+
   def test_should_round_numbers
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => "157.003"){}
     assert @output_buffer !~ /x_amount.*157.003"/
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => "157.005"){}
     assert @output_buffer =~ /x_amount.*157.01"/
   end
-		
+
   def test_all_fields
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
 
@@ -50,11 +50,11 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
                           :last_name => 'g',
                           :email => 'g@g.com',
                           :phone => '3'
-                          
+
       service.billing_address :zip => 'g',
                       :country => 'United States of America',
                       :address1 => 'g'
-      
+
       service.ship_to_address :first_name => 'g',
                                :last_name => 'g',
                                :city => '',
@@ -63,7 +63,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
                                :state => 'ut',
                                :country => 'United States of America',
                                :zip => 'g'
-                               
+
       service.invoice "516428355"
       service.notify_url "http://t/authorize_net_sim/payment_received_notification_sub_step"
       service.payment_header 'MyFavoritePal'
@@ -72,7 +72,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
       service.shipping '25.0'
       service.add_shipping_as_line_item
     }
-       
+
     all = '<INPUT TYPE=HIDDEN name="x_cust_id" value="8">
       <INPUT TYPE=HIDDEN name="x_ship_to_last_name" value="g">
       <INPUT TYPE=HIDDEN name="x_fp_timestamp" value="1206836763">
@@ -102,7 +102,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
       <INPUT TYPE=HIDDEN name="x_test_request" value="true">
       <INPUT TYPE=HIDDEN name="x_freight" value="25.0"/>
       <INPUT TYPE=HIDDEN name="x_line_item" value="Shipping<|>Shipping and Handling Cost<|>Shipping and Handling Cost<|>1<|>25.0<|>N">'
-    
+
     # clean it up a bit for parsing
     @output_buffer.gsub!("type=\"hidden\" ", "")
     for line in all.split("\n") do
@@ -119,7 +119,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
       assert @output_buffer.include?(line), ['unable to find ', line, ' ', 'in \n', @output_buffer].join(' ')
     end
   end
-  
+
   def test_custom
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
       service.add_custom_field 'abc', 'def'
@@ -127,8 +127,8 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     all = ["<input id=\"abc\" name=\"abc\" type=\"hidden\" value=\"def\" />"]
     check_inclusion all
   end
-  
-  
+
+
   def test_shipping_and_tax_line_item
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
       service.shipping 44.0
@@ -141,7 +141,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     ]
     check_inclusion all
   end
-  
+
   def test_shipping_large
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
 
@@ -152,18 +152,16 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
      <input id=\"x_ship_to_country\" name=\"x_ship_to_country\" type=\"hidden\" value=\"US\" />\n<input id=\"x_ship_to_zip\" name=\"x_ship_to_zip\" type=\"hidden\" value=\"84601\" />\n<input id=\"x_ship_to_company\" name=\"x_ship_to_company\" type=\"hidden\" value=\"company1\" />\n
      <input id=\"x_ship_to_state\" name=\"x_ship_to_state\" type=\"hidden\" value=\"TX\" />\n"
     for line in expected.split("\n") do
-      assert @output_buffer.include?(line.strip), 'expected but not found' + line 
+      assert @output_buffer.include?(line.strip), 'expected but not found' + line
     end
   end
-  
+
   def test_line_item
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
       service.add_line_item :name => 'name1', :quantity => 1, :unit_price => 1, :tax => 'true'
       service.add_line_item :name => 'name2', :quantity => '2', :unit_price => '2'
-      begin
+      assert_raise(RuntimeError) do
         service.add_line_item :name => 'name3', :quantity => '3',  :unit_price => '-3'
-	      flunk 'cant have negative price'
-      rescue
       end
       service.tax 4
       service.shipping 5
@@ -171,9 +169,9 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
       service.add_shipping_as_line_item
     }
     all = ["<input id=\"x_line_item\" name=\"x_line_item\" type=\"hidden\" value=\"Item 1<|>name1<|>name1<|>1<|>1.0<|>N\" />"]
-    check_inclusion all 
+    check_inclusion all
   end
-  
+
   def test_line_item_weird_prices
     payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
       service.add_line_item :name => 'name1', :quantity => 1, :unit_price => "1.001", :tax => 'true'
@@ -183,7 +181,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     assert @output_buffer !~ /1.001/
     assert @output_buffer =~ /1.01/
   end
-  
+
   def test_ship_to
       payment_service_for('44','8wd65QS', :service => :authorize_net_sim,  :amount => 157.0){|service|
         service.tax 4
@@ -201,7 +199,7 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
       service.customer :first_name => 'Cody',
                          :last_name => 'Fauser',
                          :phone => '(555)555-5555',
-                         :email => 'g@g.com' 
+                         :email => 'g@g.com'
 
       service.billing_address :city => 'city1',
                                 :address1 => 'g',
@@ -209,13 +207,13 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
                                 :state => 'UT',
                                 :country => 'United States of America',
                                 :zip => '90210'
-       service.invoice '#1000' 
+       service.invoice '#1000'
        service.shipping '30.00'
        service.tax '31.00'
        service.test_request 'true'
 
     }
-    
+
     expected = "<input id=\"x_cust_id\" name=\"x_cust_id\" type=\"hidden\" value=\"8\" />
 
     <input id=\"x_city\" name=\"x_city\" type=\"hidden\" value=\"city1\" />
@@ -242,5 +240,5 @@ class AuthorizeNetSimModuleTest < ActionViewHelperTest
     ActiveMerchant::Billing::Base.integration_mode = :production
     assert_equal 'https://secure.authorize.net/gateway/transact.dll', AuthorizeNetSim.service_url
   end
-  
-end 
+
+end
